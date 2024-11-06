9279ab9ca06db1d9e5572e1dd791e57e8ade77be
onekill0503 <gamemasteraji123@gmail.com>
2024-11-06 19:22:01 +0700

    feat: add dummy test

---
+++ test/Donate.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+
+contract DonateTest is Test {
+
+    function test_Donate() public {
+
+    }
+
+}
---
+++ test/Vault.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test} from "forge-std/Test.sol";
+
+contract VaultTest is Test {
+
+    function test_Vault() public {
+
+    }
+
+}
